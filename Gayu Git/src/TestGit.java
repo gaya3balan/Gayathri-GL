1ccd870a21bc81a8f0f5d3adb0c068a225283a11
prave <prave@192.168.0.47>
2022-05-15 15:03:41 -0500

    second commit

--- Gayu Git/src/TestGit.java
+++ Gayu Git/src/TestGit.java
@@ -1,10 +1,9 @@
 
-public class HelloWorld {
+public class TestGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Java");
-
+          System.out.println("testgit");
 	}
 
 }
