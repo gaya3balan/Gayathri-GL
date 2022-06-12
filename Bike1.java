0330a89e565780cdc0656a97582a6647d5ed47ef
gaya3balan <105603694+gaya3balan@users.noreply.github.com>
2022-06-12 18:54:57 -0500

    Add files via upload
    commit1

---
+++ Bike1.java
@@ -0,0 +1,14 @@
+
+public class Bike1 {
+	
+	Bike1(){System.out.println("default bike1 constructor"); }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Bike1 ob = new Bike1();
+		System.out.println(ob); // identity hashmap address of the object 
+
+	}
+
+}
