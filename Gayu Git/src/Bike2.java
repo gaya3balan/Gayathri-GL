d2f984462f4dd966931ba80214a242d69b9404ec
gaya3balan <105603694+gaya3balan@users.noreply.github.com>
2022-06-12 18:55:37 -0500

    Add files via upload
    commit1

---
+++ Gayu Git/src/Bike2.java
@@ -0,0 +1,12 @@
+
+public class Bike2 {
+	
+	Bike2(){ System.out.println("default constructor2 ");}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bike2 ob; // ob not invoking constructor as memory is not dynamically allocated 
+		//System.out.println(ob);
+	}
+
+}
